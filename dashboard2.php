68d3807225ec5ccd62a528f9a57d146c566c8485
zaskiaaprilianawanza <zaskiaapriliana1@gmail.com>
2025-11-20 20:27:00 +0700

    Commit 5 – Dashboard Penjualan: Tambah header dan array data produk

--- dashboard2.php
+++ dashboard2.php
@@ -17,3 +17,29 @@ if (!isset($_SESSION['username'])) {
 
 </body>
 </html>
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head><title>Dashboard Penjualan</title></head>
+<body>
+
+<h1>-- POLGAN MART --</h1>
+<h3>Selamat datang, <?php echo $_SESSION['username']; ?></h3>
+
+<?php
+$kode_barang  = ["001","002","003","004","005"];
+$nama_barang  = ["Laptop","Mouse","Speaker","Tas Laptop","Mouse Pad"];
+$harga_barang = [5000000,50000,100000,50000,35000];
+?>
+
+<a href="logout.php">Logout</a>
+
+</body>
+</html>
