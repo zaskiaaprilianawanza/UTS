3100781a001ff7bcb511f4e3a78a7af2cdf048f6
zaskiaaprilianawanza <zaskiaapriliana1@gmail.com>
2025-11-20 20:23:06 +0700

    Commit 3 – Proses Login dan Session: Tambah validasi login dan session

--- index.php
+++ index.php
@@ -15,3 +15,37 @@
 
 </body>
 </html>
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // login statis
+    if ($username === 'admin' && $password === '1234') {
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+<body>
+
+<h2>Form Login</h2>
+
+<?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+
+<form method="post" action="login.php">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
