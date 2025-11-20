f64685b63512e978dcd60f2053bc65efa2b03e07
zaskiaaprilianawanza <zaskiaapriliana1@gmail.com>
2025-11-20 20:21:12 +0700

    Commit 2 – Mendesain Halaman Login: Tambah form login sederhana

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h2>Form Login</h2>
+
+<form method="post" action="login.php">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
